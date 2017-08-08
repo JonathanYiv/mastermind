915f77a91183f3329d40f5addf9dcc5112a47fc5
Jonathan Yiv <JonathanYiv@gmail.com>
2017-08-07 21:11:51 -0400

    Upgraded AI to 20% win-rate

--- computer.rb
+++ computer.rb
@@ -18,33 +18,13 @@ class Computer
 	def get_guess(hints, turn) 
 		computer_guess = []
 
-=begin VERSION 1
-		if turn < 9
-			4.times do
-				computer_guess << @color_spectrum[@guess_set] 
-			end
-		
-			if turn > 1
-				@correct_colors << @color_spectrum[@guess_set - 1] if hints[12 - turn + 1].colors[0] == "green"
-			end
-
-			@guess_set += 1 if @guess_set < 8
-		else
-			if turn == 9
-				@correct_colors << @color_spectrum[@guess_set - 1] if hints[12 - turn + 1].colors[0] == "green"
-			end
-			computer_guess = @correct_colors
-			@correct_colors = @correct_colors.shuffle
-		end
-=end
-
-		#VERSION 2
+=begin	
 		case turn
 		when 1
 			4.times { computer_guess << @color_spectrum[@guess_set] }
 
 			@guess_set += 1
-		when (2..8)
+		when (2..8) 
 			4.times { computer_guess << @color_spectrum[@guess_set] }
 
 			@correct_colors << @color_spectrum[@guess_set - 1] if color_is_correct(hints, turn)
@@ -58,7 +38,34 @@ class Computer
 			computer_guess = @correct_colors
 			@correct_colors = @correct_colors.shuffle
 		end
+=end
 		
+		#VERSION 3: AI now will start guessing combinations of the correct colors as soon as it has all four. Roughly 20% win rate
+		if @correct_colors.uniq.length != 4
+			case turn
+			when 1
+				4.times { computer_guess << @color_spectrum[@guess_set] }
+
+				@guess_set += 1
+			when (2..8) 
+				4.times { computer_guess << @color_spectrum[@guess_set] }
+
+				@correct_colors << @color_spectrum[@guess_set - 1] if color_is_correct(hints, turn)
+
+				@guess_set += 1 if @guess_set < 8
+			when 9
+				@correct_colors << @color_spectrum[@guess_set - 1] if color_is_correct(hints, turn)
+				computer_guess = @correct_colors
+				@correct_colors = @correct_colors.shuffle
+			else
+				computer_guess = @correct_colors
+				@correct_colors = @correct_colors.shuffle
+			end
+		else
+			computer_guess = @correct_colors
+			@correct_colors = @correct_colors.shuffle
+		end
+
 		computer_guess
 	end
 
